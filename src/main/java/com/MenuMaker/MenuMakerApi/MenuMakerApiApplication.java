f983119cedaecb5c30634f167781d204b855967e
MalBourbie <MalBourbie@gmail.com>
2025-03-14 17:37:10 +0100

    initial commit

---
+++ src/main/java/com/MenuMaker/MenuMakerApi/MenuMakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.MenuMaker.MenuMakerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuMakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuMakerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MenuMaker/MenuMakerApi/MenuMakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MenuMaker.MenuMakerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuMakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
