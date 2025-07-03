344726d952fe37dde04d3d753399b5cde6679c5c
MalBourbie <MalBourbie@gmail.com>
2025-07-03 19:05:34 +0200

    AuthService test.

--- src/test/java/com/MenuMaker/MenuMakerApi/serviceTest/AuthServiceTest.java
+++ src/test/java/com/MenuMaker/MenuMakerApi/serviceTest/AuthServiceTest.java
@@ -1,17 +1,12 @@
 package com.MenuMaker.MenuMakerApi.serviceTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import java.util.Base64;
-import java.util.Date;
-
-import javax.crypto.SecretKey;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,116 +15,45 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.MenuMaker.MenuMakerApi.model.DAO.UserModel;
 import com.MenuMaker.MenuMakerApi.repository.UserRepository;
 import com.MenuMaker.MenuMakerApi.service.AuthService;
-import com.MenuMaker.MenuMakerApi.service.TokenService;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 
 @SpringBootTest
 public class AuthServiceTest {
-    private String secret = "vE5v2pd7VWFA8DOhDBO+5Ecirg95kcs+14+iWUFP0S4=";
-    private SecretKey secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secret));
 
     private UserRepository userRepository;
     private AuthService authService;
-    private TokenService tokenService;
 
     @BeforeEach
     void setUp() {
         userRepository = mock(UserRepository.class);
         authService = new AuthService(userRepository);
-        tokenService = mock(TokenService.class);
     }
 
     @Test
-    void shortTimeTokenShouldGeneratWithSuccess() throws Exception {
-        String userEmail = "test@gmail.com";
-
-        // act
-        String token = tokenService.createShortTimeToken(userEmail);
-
-        assertNotNull(token, "Token must not be null");
-
-        Claims claims = Jwts.parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
-        // email validation
-        assertEquals(userEmail, claims.getSubject(), "Expect token payload to be userEmail");
-
-        // expiration validation
-        long expiration = claims.getExpiration().getTime();
-        long expectedExpiration = claims.getIssuedAt().getTime() + (10 * 60 * 1000);
-
-        assertEquals(expectedExpiration, expiration, "Token must have a 10min expiration");
-    }
-
-    @Test
-    void longTimeTokenShouldGenerateWithSuccess() throws Exception {
-        String userEmail = "test@gmail.com";
-
-        // act
-        String token = tokenService.createLongTimeToken(userEmail);
-
-        assertNotNull(token, "Token must not be null");
-
-        Claims claims = Jwts.parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
-        // email validation
-        assertEquals(userEmail, claims.getSubject(), "Expect token payload to be userEmail");
-
-        // expiration validation
-        long expiration = claims.getExpiration().getTime();
-        long expectedExpiration = claims.getIssuedAt().getTime() + (6 * 60 * 60 * 1000);
-
-        assertEquals(expectedExpiration, expiration, "Token must have a 6 hours expiration");
-    }
-
-    @Test
-    void getEmailFromTokenWithSuccess() throws Exception {
-        String userEmail = "test@gmail.com";
-
-        // act
-        String token = tokenService.createShortTimeToken(userEmail);
-
-        String email = tokenService.getEmailFromToken(token);
-
-        assertEquals(userEmail, email);
-    }
-
-    @Test
-    void emailExistInDB() throws Exception {
+    void registerIfEmailNotRegistered_withSucces() {
         String email = "test@gmail.com";
 
-        when(userRepository.existsByEmail(email)).thenReturn(true);
-        // act
-        authService.isEmailRegistered(email);
+        UserModel newUser = new UserModel.Builder().email(email).build();
 
-        verify(userRepository, times(1)).existsByEmail(email);
-    }
-
-    @Test
-    void registerUserWithSuccess() throws Exception {
-        String email = "test@gmail.com";
-
-        UserModel user = new UserModel();
-        user.setEmail(email);
-        user.setCreatedAt(new Date());
-        user.setUpdatedAt(new Date());
-        user.setRestaurantName(null);
-
-        when(userRepository.save(any(UserModel.class))).thenReturn(user);
+        when(userRepository.existsByEmail(email)).thenReturn(false);
+        when(userRepository.save(any(UserModel.class))).thenReturn(newUser);
 
         // act
-        UserModel userCreated = authService.registerUser(email);
+        UserModel userCreated = authService.registerIfEmailNotRegistered(email);
 
-        assertNotNull(userCreated, "userCreated should not be null");
-        assertEquals(user.getEmail(), userCreated.getEmail(), "Emails should match");
+        assertEquals(newUser.getEmail(), userCreated.getEmail(), "Emails should match");
         verify(userRepository, times(1)).save(any(UserModel.class));
+    }
+
+    @Test
+    void registerIfEmailNotRegistered_alreadyRegistered() {
+        String email = "test@gmail.com";
+
+        when(userRepository.existsByEmail(email)).thenReturn(false);
+
+        // act
+        UserModel userCreated = authService.registerIfEmailNotRegistered(email);
+
+        assertNull(userCreated, "Must return null when is already registered");
+        verify(userRepository, times(1)).existsByEmail(any(String.class));
     }
 }
